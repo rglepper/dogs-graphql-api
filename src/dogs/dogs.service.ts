4d1cec0e7f02dab458de78bf232c12384767c02d
Rafa Garcia-Lepper <rafael.g.lepper@gmail.com>
2020-04-16 14:57:04 +0100

    Add create method to the service
    This method takes an object defined in the graphql input type. It creates an instance of the Mongoose model using the arguments and saves it to db. Returns a promise in the shape of the dog model

--- src/dogs/dogs.service.ts
+++ src/dogs/dogs.service.ts
@@ -2,9 +2,16 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Dog } from './interface/dog.interface';
+import { DogInput } from './inputs/dog.input';
 
 
 @Injectable()
 export class DogsService {
 	constructor(@InjectModel('Dog') private readonly dogModel: Model<Dog> ) {}
+
+	async createDog(createDogDto: DogInput): Promise<Dog> {
+		const dog = new this.dogModel(createDogDto);
+		return await dog.save();
+	}
+
 }
