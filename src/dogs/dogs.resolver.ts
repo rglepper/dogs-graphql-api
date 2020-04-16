f84d15f7d2c373823f1a673461de9740fdc653ff
Rafa Garcia-Lepper <rafael.g.lepper@gmail.com>
2020-04-16 15:17:04 +0100

    Resolve query to get all

--- src/dogs/dogs.resolver.ts
+++ src/dogs/dogs.resolver.ts
@@ -1,11 +1,17 @@
-import { Resolver, Query } from '@nestjs/graphql';
-import { AppService } from '../app.service';
+import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
+import { DogsService } from './dogs.service';
+import { Dog } from './interface/dog.interface';
+import { DogModel } from './model/dog.model';
 
 @Resolver()
 export class DogsResolver {
 
-	constructor(private readonly appService: AppService) {}
+	constructor(private readonly dogsService: DogsService) {}
 
+	@Query(() => [DogModel])
+	async getDogs(): Promise<Dog[]> {
+		return this.dogsService.getDogs();
+	}
 
 	@Query(() => String)
 	async hello() {
