0edd984140fde1878829864eb549613972578fa5
Rafa Garcia-Lepper <rafael.g.lepper@gmail.com>
2020-04-16 11:54:51 +0100

    Connect to db using Mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,21 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { AppService } from './app.service';
 import { join } from 'path';
 import { DogsModule } from './dogs/dogs.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     GraphQLModule.forRoot({
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:u3J2gItSQbI7mXI6@presenter-kfpn3.mongodb.net/test?retryWrites=true&w=majority',
+      {
+        dbName: 'dogs',
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      }
+    ),
     DogsModule
   ],
   providers: [AppService],
