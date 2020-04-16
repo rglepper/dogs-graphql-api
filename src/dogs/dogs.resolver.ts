65b8168fbb2d7a33ce7e9d531595a4ea7d75c7e4
Rafa Garcia-Lepper <rafael.g.lepper@gmail.com>
2020-04-16 15:17:54 +0100

    Resolve mutation to create a new dog

--- src/dogs/dogs.resolver.ts
+++ src/dogs/dogs.resolver.ts
@@ -2,6 +2,8 @@ import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { DogsService } from './dogs.service';
 import { Dog } from './interface/dog.interface';
 import { DogModel } from './model/dog.model';
+import { DogInput } from './inputs/dog.input';
+
 
 @Resolver()
 export class DogsResolver {
@@ -13,8 +15,10 @@ export class DogsResolver {
 		return this.dogsService.getDogs();
 	}
 
-	@Query(() => String)
-	async hello() {
-		return this.appService.getHello();
+	@Mutation(returns => DogModel)
+	async createDog(
+		@Args('dogData') dogData: DogInput
+	): Promise<Dog> {
+		return await this.dogsService.createDog(dogData)
 	}
 }
